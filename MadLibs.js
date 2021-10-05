9aeebe77574fe44694791f56b1d7c2cc5016cb3f
nmahern <79818589+nmahern@users.noreply.github.com>
2021-10-04 22:51:57 -0400

    Add files via upload

---
+++ MadLibs.js
@@ -0,0 +1,11 @@
+
+function upper(el) {
+    var str = el.value;
+    el.value = str.toUpperCase();
+    }
+
+    upper(document.getElementById("candy"));
+    upper(document.getElementById("holiday"));
+    upper(document.getElementById("season"));
+    upper(document.getElementById("location"));
+    upper(document.getElementById("color"));
